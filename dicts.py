794ede30273d3b2f3bd2d3b5135c04abaa8be361
Harshith Kulkarni <harshithkulkarni@gmail.com>
2021-06-27 00:43:01 +0530

    Add files via upload

---
+++ dicts.py
@@ -0,0 +1,14 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+print(thisdict["brand"])
+
+# Changing values of keys
+
+thisdict["brand"] = "Lamborghini"
+
+print(thisdict)
